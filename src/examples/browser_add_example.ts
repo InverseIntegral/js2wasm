5c52bb119c11dc5449a3e446043d7f7a8f7224da
SeboCode <mike-marti@outlook.com>
2018-09-22 19:24:11 +0200

    Extract dom and webassembly instance creation

--- src/examples/browser_add_example.ts
+++ src/examples/browser_add_example.ts
@@ -1,14 +1,25 @@
-window.onload = () => {
-    (document.getElementById('calculate') as HTMLElement).addEventListener('click', () => {
-        const left: number = Number((document.getElementById('left') as HTMLInputElement).value);
-        const right: number = Number((document.getElementById('right') as HTMLInputElement).value);
-        (document.getElementById('result') as HTMLElement).innerHTML = String(calculate(left, right));
-    });
-};
+import Instance = WebAssembly.Instance;
 
-function calculate(left: number, right: number): number {
+const wasmInstance: Instance = createWebassemblyInstance();
+
+function createWebassemblyInstance() {
     const binary: ArrayBuffer = new Uint8Array([0, 97, 115, 109, 1, 0, 0, 0, 1, 7, 1, 96, 2, 127, 127, 1, 127,
         3, 2, 1, 0, 7, 7, 1, 3, 97, 100, 100, 0, 0, 10, 9, 1, 7, 0, 32, 0, 32, 1, 106, 11]).buffer;
-    const wasm: any = new WebAssembly.Instance(new WebAssembly.Module(binary), {});
-    return wasm.exports.add(left, right);
+    return new WebAssembly.Instance(new WebAssembly.Module(binary), {});
 }
+
+function calculate(left: number, right: number): number {
+    return wasmInstance.exports.add(left, right);
+}
+
+window.onload = () => {
+    const leftInputElement = document.getElementById('left') as HTMLInputElement;
+    const rightInputElement = document.getElementById('right') as HTMLInputElement;
+    const resultElement = document.getElementById('result') as HTMLElement;
+
+    (document.getElementById('calculate') as HTMLElement).addEventListener('click', () => {
+        const left: number = Number(leftInputElement.value);
+        const right: number = Number(rightInputElement.value);
+        resultElement.innerHTML = String(calculate(left, right));
+    });
+};

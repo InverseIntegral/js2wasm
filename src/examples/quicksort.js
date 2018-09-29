fb8f0e5fc6545012a375ce829a0ea66b415bde8a
Inverse Integral <inverseintegral42@gmail.com>
2018-09-29 10:26:05 +0200

    Add quicksort example

---
+++ src/examples/quicksort.js
@@ -0,0 +1,47 @@
+function swap(items, first, second) {
+    var temp = items[first];
+    items[first] = items[second];
+    items[second] = temp;
+}
+
+function partition(items, left, right) {
+    var pivot = items[Math.floor((right + left) / 2)];
+    var i = left;
+    var j = right;
+
+    while (i <= j) {
+        while (items[i] < pivot) {
+            i++;
+        }
+
+        while (items[j] > pivot) {
+            j--;
+        }
+
+        if (i <= j) {
+            swap(items, i, j);
+            i++;
+            j--;
+        }
+    }
+
+    return i;
+}
+
+function quickSort(items, left, right) {
+    if (items.length <= 1) {
+        return items;
+    }
+
+    var index = partition(items, left, right);
+
+    if (left < index - 1) {
+        quickSort(items, left, index - 1);
+    }
+
+    if (index < right) {
+        quickSort(items, index, right);
+    }
+
+    return items;
+}

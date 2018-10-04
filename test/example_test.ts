d2673f03f8e4b79599a6c6b093d495280fd7634a
SeboCode <mike-marti@outlook.com>
2018-10-04 21:53:11 +0200

    Remove suprefluous example typescript file

--- test/example_test.ts
+++
@@ -1,8 +0,0 @@
-import { expect } from 'chai';
-import adder from '../src/example';
-
-describe('Example function', () => {
-   it('should return 42', () => {
-     expect(adder(21, 21)).to.equal(42);
-   });
-});

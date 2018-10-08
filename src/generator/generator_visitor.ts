b0722d039227ad39404a00025db7fcd4bde94e8f
Inverse Integral <inverseintegral42@gmail.com>
2018-10-08 14:41:25 +0200

    Add generator visitor class

---
+++ src/generator/generator_visitor.ts
@@ -0,0 +1,61 @@
+import {
+    BinaryExpression,
+    BlockStatement,
+    BooleanLiteral,
+    Identifier,
+    IfStatement,
+    NumericLiteral,
+    ReturnStatement,
+    UnaryExpression,
+} from '@babel/types';
+import {Expression, Module, Statement} from 'binaryen';
+import Visitor from '../visitor';
+
+class GeneratorVisitor extends Visitor {
+
+    private readonly module: Module;
+
+    private statements: Statement[] = [];
+    private expressions: Expression[] = [];
+
+
+    constructor(module: Module) {
+        super();
+        this.module = module;
+    }
+
+    protected visitIdentifier(node: Identifier) {
+
+    }
+
+    protected visitNumericLiteral(node: NumericLiteral) {
+
+    }
+
+    protected visitBooleanLiteral(node: BooleanLiteral) {
+
+    }
+
+    protected visitReturnStatement(node: ReturnStatement) {
+
+    }
+
+    protected visitUnaryExpression(node: UnaryExpression) {
+
+    }
+
+    protected visitBinaryExpression(node: BinaryExpression) {
+
+    }
+
+    protected visitIfStatement(node: IfStatement) {
+
+    }
+
+    protected visitBlockStatement(node: BlockStatement) {
+
+    }
+
+}
+
+export default GeneratorVisitor;
